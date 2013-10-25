47cb038c7e19d3dfe73f19766b3535cef55fd4a3
David Luposchainsky <dluposchainsky@gmail.com>
2013-10-25 15:34:09 +0200

    Refurbish Bootstrap.hs

--- src/Bootstrap.hs
+++ src/Bootstrap.hs
@@ -29,47 +29,33 @@ instance Exception BadBootstrapResponse
 
 
 
--- | Send out a BootstrapRequest to a bootstrap server and handle the response.
-bootstrap :: Config -> PortNumber -> IO HostName -- TODO: Make it an Either Error Hostname so the problem can be printed
+-- | Send out a 'BootstrapRequest' to a bootstrap server and handle the
+--   response.
+bootstrap :: Config
+          -> Int         -- ^ Own server port
+          -> IO HostName -- ^ Own hostname
 bootstrap config port = do
 
-      -- TODO: Add a function to find a random bootstrap nodes
-
-      -- Send out signal to the bootstrap node
       bNode <- getBootstrapServer
 
-
-      -- Don't recurse directly in case of failure so that bracket can close
-      -- the handle properly. Instead, bind the result to an identifier, and
-      -- check it after the bracketing.
-      --let try' :: IO a -> IO (Either BadBootstrapResponse a)
-      --    try' = try
-      result <- try $ bracket (connectToNode bNode) hClose $ \h -> do
-
-            -- See note [Why send port?]
-            request' h (BootstrapRequest port) >>= \case
-                  (YourHostIs host) -> return host
-                  _                 -> throwIO BadBootstrapResponse
+      result <- connectToNode bNode $ \(socket, _) -> do
+            request' socket (BootstrapRequest port) >>= \case
+                  Just (YourHostIs host) -> return (Just host)
+                  Just _ -> putStrLn "Bad bootstrap server response. Bug!" >> return Nothing
+                  Nothing -> return Nothing
 
       case result of
-            Left (SomeException e) -> do
-                  putStrLn $ "Bootstrap failed (" ++ show e ++ ")\
-                             \  (If the bootstrap server is valid,\
-                             \ this is likely a bug.)"
+            Just host -> return host
+            Nothing -> do
+                  putStrLn $ "Bootstrap failed. This is likely a bug."
                   threadDelay (_mediumTickRate config)
                   bootstrap config port
-            Right r -> return r
-
--- [Why send port?]
---
--- To issue EdgeRequests in the name of the node to be bootstrapped, the server
--- has to be aware of a return address. While it can deduce the hostname from
--- the incoming connection, the port of the new node's server is unknown.
--- for that reason, the node has to provide it explicitly.
 
 
 
--- | Finds the address of a suitable bootstrap server.
+
+
+-- | Find the address of a suitable bootstrap server.
 getBootstrapServer :: IO To
 getBootstrapServer = return . To $ Node "localhost" 20000
 -- TODO: Make bootstrap server selection a little more complex :-)
