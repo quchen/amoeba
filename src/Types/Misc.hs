8b519d897fecf55314d55b5242ab17469ed16666
David Luposchainsky <dluposchainsky@gmail.com>
2013-11-07 11:39:11 +0100

    Split Types.hs in submodules

---
+++ src/Types/Misc.hs
@@ -0,0 +1,35 @@
+-- | Types that don't fit in any of the specialized modules.
+
+module Types.Misc where
+
+
+
+-- | How many messages should be printed?
+data Verbosity = Chatty  -- ^ *Everything*, e.g. passing bounces, keep-alive
+                         --   signals
+               | Debug   -- ^ Various status messages, e.g. gaining and losing
+                         --   neighbours
+               | Default -- ^ Useful for normal execution, e.g. node deficit,
+                         --   chat messages
+               | Quiet   -- ^ Only messages intended for display, i.e. chat
+               | Mute    -- ^ Nothing, node just serves as a network helper
+      deriving (Eq, Ord, Show)
+      -- Note: Order matters in order to make `myVerbosity > x` work!
+
+
+
+-- | Unique identifier for upstream nodes.
+newtype From = From { getFrom :: Integer }
+      deriving (Eq, Ord)
+
+instance Show From where
+      show (From i) = '#' : show i
+
+
+
+-- | Encodes in what relationship two nodes stand to each other
+data NodeRelationship = IsSelf
+                      | IsDownstreamNeighbour
+                      | IsUnrelated
+                   -- | IsUpstreamNeighbour -- Currently unused
+
