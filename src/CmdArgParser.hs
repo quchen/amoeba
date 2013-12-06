56eb6794be4eb60d01c3e43e99a54b81abf0b033
David Luposchainsky <dluposchainsky@gmail.com>
2013-12-06 06:40:10 +0100

    Refactor arg parser
    - Compatibility with 0.7
    - Eliminate ($)

--- src/CmdArgParser.hs
+++ src/CmdArgParser.hs
@@ -8,11 +8,16 @@ import Text.Read (readEither)
 
 import qualified Types as T
 
-parseArgs = execParser opts
-      where opts = info (helper <*> config) $
-                   fullDesc
-                <> progDesc "Amoeba client"
-                <> header "Launch a single node in an Amoeba network"
+
+
+parseArgs :: IO T.Config
+parseArgs = execParser parser
+      where parser = info (helper <*> config) infoMod
+            infoMod = mconcat
+                  [ fullDesc
+                  , progDesc "Amoeba client"
+                  , header "Launch a single node in an Amoeba network"
+                  ]
 
 
 
@@ -55,57 +60,67 @@ config = T.Config
 
 
 port :: Parser Int
-port = option $ mconcat [ long    "port"
-                        , short   'p'
-                        , metavar "PORT"
-                        , help    "Server port"
-                        ]
+port = (option . mconcat) [ long    "port"
+                          , short   'p'
+                          , metavar "PORT"
+                          , help    "Server port"
+                          ]
+
+
 
 minNeighbours :: Parser Int
-minNeighbours = nullOption $ mconcat
+minNeighbours = (nullOption . mconcat)
       [ reader positive
       , showDefault
-      , value $ T._maxNeighbours defaultConfig
+      , value   (T._maxNeighbours defaultConfig)
       , long    "maxn"
       , metavar "<INT > 0>"
       , help    "Minimum amount of neighbours (up-/downstream separate)"
       ]
 
+
+
 maxNeighbours :: Parser Int
-maxNeighbours = nullOption $ mconcat
+maxNeighbours = (nullOption . mconcat)
       [ reader positive
       , showDefault
-      , value $ T._minNeighbours defaultConfig
+      , value   (T._minNeighbours defaultConfig)
       , long    "minn"
       , metavar "<INT > 0>"
       , help    "Maximum amount of neighbours (up-/downstream separate)"
       ]
 
+
+
 maxChanSize :: Parser Int
-maxChanSize = nullOption $ mconcat
+maxChanSize = (nullOption . mconcat)
       [ reader positive
       , showDefault
-      , value $ T._maxChanSize defaultConfig
+      , value   (T._maxChanSize defaultConfig)
       , long    "chansize"
       , metavar "<INT > 0>"
       , help    "Maximum communication channel size"
       ]
 
+
+
 bounces :: Parser Word
-bounces = nullOption $ mconcat
+bounces = (nullOption . mconcat)
       [ reader nonnegative
       , showDefault
-      , value $ T._bounces defaultConfig
+      , value   (T._bounces defaultConfig)
       , long    "bounces"
       , metavar "<INT >= 0>"
       , help    "Minimum edge search hard bounces"
       ]
 
+
+
 maxSoftBounces :: Parser Word
-maxSoftBounces = nullOption $ mconcat
+maxSoftBounces = (nullOption . mconcat)
       [ reader positive
       , showDefault
-      , value $ T._maxSoftBounces defaultConfig
+      , value   (T._maxSoftBounces defaultConfig)
       , long    "hbounce"
       , metavar "<INT > 0>"
       , help    "Maximum edge search soft bounces"
@@ -113,41 +128,46 @@ maxSoftBounces = nullOption $ mconcat
 
 
 
-
 acceptP :: Parser Double
-acceptP = nullOption $ mconcat
+acceptP = (nullOption . mconcat)
       [ reader probability
       , showDefault
-      , value $ T._acceptP defaultConfig
+      , value   (T._acceptP defaultConfig)
       , long    "acceptp"
       , metavar "<0 < p <= 1>"
       , help    "Edge request soft bounce acceptance probability"
       ]
 
-tickRate :: Char -> String -> (T.Config -> Int) -> Parser Int
-tickRate shortName name getter = nullOption $ mconcat
-      [ reader positive
-      , showDefaultWith $ \val -> show val
-      , value $ getter defaultConfig
-      , long  $ shortName : "tick"
-      , metavar "MILLISECONDS"
-      , help  $ "Tick rate of " ++ name ++ " loops"
-      ]
 
-poolTimeout :: Parser Double
-poolTimeout = nullOption $ mconcat
+
+tickRate :: Char -> String -> (T.Config -> Int) -> Parser Int
+tickRate shortName name getter = (nullOption . mconcat)
       [ reader positive
       , showDefault
-      , value $ T._poolTimeout defaultConfig
+      , value   (getter defaultConfig)
+      , long    (shortName : "tick")
+      , metavar "MILLISECONDS"
+      , help    ("Tick rate of " ++ name ++ " loops")
+      ]
+
+
+
+poolTimeout :: Parser Double
+poolTimeout = (nullOption . mconcat)
+      [ reader positive
+      , showDefault
+      , value   (T._poolTimeout defaultConfig)
       , long    "timeout"
       , metavar "SECONDS"
       , help    "Timeout threshold"
       ]
 
+
+
 verbosity :: Parser T.Verbosity
-verbosity = nullOption $ mconcat
+verbosity = (nullOption . mconcat)
       [ reader readVerbosity
-      , value $ T._verbosity defaultConfig
+      , value   (T._verbosity defaultConfig)
       , long    "verbosity"
       , metavar "<mute|quiet|default|debug|chatty>"
       , help    "Verbosity level, increasing from left to right"
@@ -155,35 +175,36 @@ verbosity = nullOption $ mconcat
 
 
 
--- | Reader for a Double between 0 and 1
-probability :: (Num a, Ord a, Read a) => String -> Either ParseError a
+-- | 0 <= p <= 1
+probability :: (Num a, Ord a, Read a) => String -> ReadM a
 probability x = case readEither x of
-      Right x' | x' >= 0 && x' <= 1 -> Right x'
-      Right _  -> Left . ErrorMsg $ "Bad probability " ++ x ++ "; 0 <= p <= 1"
-      Left _   -> Left . ErrorMsg $ "Parse error on double " ++ x
+      Right x' | x' >= 0 && x' <= 1 -> pure x'
+      Right _  -> readerError ("Bad probability " ++ x ++ "; 0 <= p <= 1")
+      Left  _  -> readerError ("Parse error on double " ++ x)
 
 
 
-positive :: (Num a, Ord a, Read a) => String -> Either ParseError a
+positive :: (Num a, Ord a, Read a) => String -> ReadM a
 positive x = case readEither x of
-      Right x' | x' > 0 -> Right x'
-      Right _  -> Left . ErrorMsg $ "Positive number expected ( " ++ x ++ " given)"
-      Left _   -> Left . ErrorMsg $ "Parse error on integer " ++ x
+      Right x' | x' > 0 -> pure x'
+      Right _  -> readerError ("Positive number expected ( " ++ x ++ " given)")
+      Left  _  -> readerError ("Parse error on integer " ++ x)
 
 
 
-nonnegative :: (Num a, Ord a, Read a) => String -> Either ParseError a
+nonnegative :: (Num a, Ord a, Read a) => String -> ReadM a
 nonnegative x = case readEither x of
-      Right x' | x' >= 0 -> Right x'
-      Right _  -> Left . ErrorMsg $ "Nonnegative number expected ( " ++ x ++ " given)"
-      Left _   -> Left . ErrorMsg $ "Parse error on integer " ++ x
+      Right x' | x' >= 0 -> pure x'
+      Right _  -> readerError ("Nonnegative number expected ( " ++ x ++ " given)")
+      Left  _  -> readerError ("Parse error on integer " ++ x)
 
 
-readVerbosity :: String -> Either ParseError T.Verbosity
+
+readVerbosity :: String -> ReadM T.Verbosity
 readVerbosity x = case map toLower x of
-      "mute"    -> Right T.Mute
-      "quiet"   -> Right T.Quiet
-      "default" -> Right T.Default
-      "debug"   -> Right T.Debug
-      "chatty"  -> Right T.Chatty
-      _         -> Left . ErrorMsg $ "Unrecognized verbosity level \"" ++ x ++ "\""
+      "mute"    -> pure T.Mute
+      "quiet"   -> pure T.Quiet
+      "default" -> pure T.Default
+      "debug"   -> pure T.Debug
+      "chatty"  -> pure T.Chatty
+      _else     -> readerError ("Unrecognized verbosity level \"" ++ x ++ "\"")
